b391a8114c1a9338210f1f719672ea821e886a68
sunilkumarc <sunilkumar.c682@gmail.com>
2013-08-30 19:31:44 +0530

    Uninstallation file

---
+++ remove.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+sudo rm -f /usr/bin/alarm /opt/alarm-manager/alarm.py /usr/bin/alarm-uninstall /opt/alarm-manager/gui.py /opt/alarm-manager/remove.sh 
+sudo rmdir /opt/alarm-manager/ 
+echo "Alarm Manager successfully uninstalled."
